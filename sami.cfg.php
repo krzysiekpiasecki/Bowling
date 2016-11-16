6afe6f166987e414ff0274408687f7b72391d295
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2016-11-16 09:34:22 +0100

    Add sami config

---
+++ sami.cfg.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'vendor/autoload.php';
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('./src');
+
+return new Sami(
+    $iterator,
+    array(
+        'build_dir' => './docs/api',
+        'cache_dir' => './var/cache/sami',
+        'title' => 'BowlingGame',
+        'theme' => 'default',
+        'include_parent_data' => true,
+        'default_opened_level' => 2,
+    )
+);
